f4f3548bb25dfa9dace7e4ecba54d2ae3a45c1fb
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2018-05-02 18:00:32 +0300

    Add task4

---
+++ Task4-Winservices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace UselessWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new UselessService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Task4-Winservices/Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using System.ServiceProcess;
+
+namespace UselessWindowsService
+{
+    public partial class UselessService : ServiceBase
+    {
+        private Timer _timer;
+
+        public UselessService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer();
+            _timer.Interval = 30 * 1000;
+            _timer.Elapsed += (object sender, ElapsedEventArgs e) => ShowMessage("Hello from Useless Windows Service. Thanks for choosing us!");
+            _timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            _timer.Enabled = false;
+            ShowMessage("Good bye from Useless Windows Service. Thanks for choosing us!");
+        }
+
+        private void ShowMessage(string text)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\log.txt", true))
+                {
+                    sw.WriteLine(text);
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
