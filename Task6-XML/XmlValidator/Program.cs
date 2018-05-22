78fc1f5a5ca442565cfb7e9a51d1f6c43e4b0a5a
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2018-05-22 16:03:28 +0300

    Initial task code

---
+++ Task6-XML/XmlValidator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var readerSettings = new XmlReaderSettings();
+            readerSettings.Schemas.Add("http://library.by/catalog", "books.xsd");
+            readerSettings.ValidationType = ValidationType.Schema;
+            readerSettings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
+
+            var books = XmlReader.Create("books.xml", readerSettings);
+
+            while (books.Read()) { }
+        }
+
+        static void booksSettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                Console.Write("WARNING: ");
+                Console.WriteLine(e.Message);
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                Console.Write("ERROR: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
